84cb98368107ea1e3962985a43bc33a6202a7454
acerone85 <andrea.cerone@gmail.com>
2024-09-12 12:55:29 +0100

    Placate clippy

--- crates/services/executor/src/executor.rs
+++ crates/services/executor/src/executor.rs
@@ -191,7 +191,7 @@ impl TransactionsSource for OnceTransactionsSource {
         let transactions: &mut Vec<MaybeCheckedTransaction> = lock.as_mut();
         // Avoid panicking if we request more transactions than there are in the vector
         let transactions_limit = (transactions_limit as usize).min(transactions.len());
-        transactions.drain(..transactions_limit as usize).collect()
+        transactions.drain(..transactions_limit).collect()
     }
 }
 
